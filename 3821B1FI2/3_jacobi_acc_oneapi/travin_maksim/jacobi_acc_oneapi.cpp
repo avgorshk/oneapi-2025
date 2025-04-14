e6e46d7871e467478ef22dcbf1723f4ae81a3b40
Максим <stm_03@mail.ru>
2025-04-14 22:55:26 +0300

    jacobi_acc_oneapi task

---
+++ 3821B1FI2/3_jacobi_acc_oneapi/travin_maksim/jacobi_acc_oneapi.cpp
@@ -0,0 +1,7 @@
+#include "jacobi_acc_oneapi.h"
+
+std::vector<float> JacobiAccONEAPI(
+    const std::vector<float> a, const std::vector<float> b,
+    float accuracy, sycl::device device) {
+    // Place your implementation here
+}
