bc2d95ef84e8085713e8212e3a1bd4060d2789f7
Nikita Ivanov <87970276+Atikin-NT@users.noreply.github.com>
2025-04-16 18:15:30 +0000

    fix lab 1

--- 3821B1FI3/1_permutations_cxx/ivanov_nikita/permutations_cxx.cpp
+++ 3821B1FI3/1_permutations_cxx/ivanov_nikita/permutations_cxx.cpp
@@ -1,25 +1,27 @@
 #include "permutations_cxx.h"
 
 #include <algorithm>
+#include <unordered_map>
 
 void Permutations(dictionary_t& dictionary) {
-    std::map<std::string, std::vector<std::string>> grouped;
+    std::unordered_map<std::string, std::vector<std::string>> sorted_map;
 
-    for (const auto& entry : dictionary) {
-        std::string sorted_key = entry.first;
-        std::sort(sorted_key.begin(), sorted_key.end());
-        grouped[sorted_key].push_back(entry.first);
+    for (const auto& [str, _] : dictionary) {
+        std::string sorted_str = str;
+        std::sort(sorted_str.begin(), sorted_str.end());
+        sorted_map[sorted_str].push_back(str);
     }
 
-    for (auto& entry : dictionary) {
-        std::string sorted_key = entry.first;
-        std::sort(sorted_key.begin(), sorted_key.end());
-        std::vector<std::string>& permutations = grouped[sorted_key];
+    for (auto& [sorted_str, original_strings] : sorted_map) {
+        std::reverse(original_strings.begin(), original_strings.end());
+        for (const auto& original_str : original_strings) {
+            dictionary[original_str] = original_strings;
+        }
+    }
 
-        permutations.erase(std::remove(permutations.begin(), permutations.end(), entry.first), permutations.end());
-
-        std::sort(permutations.begin(), permutations.end(), std::greater<std::string>());
-
-        entry.second = permutations;
+    for (auto& [str, perms] : dictionary) {
+        perms.erase(
+            std::remove(perms.begin(), perms.end(), str),
+            perms.end());
     }
 }
