f45b406b2acb567ae0d75ae32bcbda7f2cdc7890
OlgaTyulkina <yalo2001q@yandex.ru>
2025-04-20 17:36:37 +0300

    commit_2

--- 3821B1FI3/2_integral_oneapi/tyulkina_olga/integral_oneapi.cpp
+++ 3821B1FI3/2_integral_oneapi/tyulkina_olga/integral_oneapi.cpp
@@ -1,30 +1,33 @@
 #include "integral_oneapi.h"
-#include <range.hpp>
-#include <reduction.hpp>
 
 float IntegralONEAPI(float start, float end, int count, sycl::device device) {
-  float ans = 0.0f;
 
-  {
-    sycl::buffer<float> buf_ans(&ans, 1);
+  float sum_integral = 0.0f;
+  float dx = (end - start) / count;
+  float dy = (end - start) / count;
+  try {
+    sycl::queue device_queue{device};
+    sycl::buffer<float> buf_sum_integral(&sum_integral, sycl::range<1>(1));
 
-    sycl::queue queue(device);
+    device_queue
+        .submit([&](sycl::handler &cgh) {
+          auto sumReduction =
+              sycl::reduction(buf_sum_integral, cgh, sycl::plus<>());
+          cgh.parallel_for<class Rieman_Integral_kernel>(
+              sycl::range<2>(count, count), sumReduction,
+              [=](sycl::item<2> item, auto &sum_integral) {
+                float x_mid = start + (item[0] + 0.5f) * dx;
+                float y_mid = start + (item[1] + 0.5f) * dy;
 
-    queue.submit([&](sycl::handler &cgh) {
-      auto reduction = sycl::reduction(buf_ans, cgh, sycl::plus<>());
-
-      cgh.parallel_for(sycl::range<2>(count, count), reduction, [=](sycl::id<2> id, auto& sum) {
-        float Xi = start + (end - start) / count * id.get(0);
-        float Xi_1 = start + (end - start) / count * (id.get(0) + 1);
-        float Yi = start + (end - start) / count * id.get(1);
-        float Yi_1 = start + (end - start) / count * (id.get(1) + 1);
-        sum += sycl::sin((Xi + Xi_1) / 2.0f) * sycl::cos((Yi + Yi_1) / 2.0f) * (Xi_1 - Xi) * (Yi_1 - Yi);
-      });
-    });
-
-    queue.wait();
+                sum_integral += sycl::sin(x_mid) * sycl::cos(y_mid) * dx * dy;
+              });
+        })
+        .wait();
+  } catch (sycl::exception e) {
+    std::cerr << e.what() << std::endl;
+  } catch (...) {
+    std::cerr << "Global Error!" << std::endl;
   }
 
-
-  return ans;
+  return sum_integral;
 }
