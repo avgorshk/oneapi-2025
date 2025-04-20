4b6301edf9dcd499213084b7b3e1f3c3743faeba
Kirill23323 <KirillProkofev12345@yandex.ru>
2025-04-20 14:39:47 +0300

    task1

---
+++ 3821B1FI3/1_permutations_cxx/prokofev_kirill/permutations_cxx.cpp
@@ -0,0 +1,29 @@
+// Copyright (c) 2025 Prokofev Kirill
+#include "permutations_cxx.h"
+
+#include <unordered_map>
+#include <algorithm>
+#include <iterator>
+
+void Permutations(dictionary_t& dictionary) {
+  std::unordered_map<std::string, std::vector<dictionary_t::iterator>> tmp;
+  for (auto it = dictionary.begin(); it != dictionary.end(); ++it) {
+    std::string key = it->first;
+    std::sort(key.begin(), key.end());
+    tmp[key].push_back(it);
+  }
+  for (auto it = dictionary.begin(); it != dictionary.end(); ++it) {
+    std::string key = it->first;
+    std::sort(key.begin(), key.end());
+    const std::vector<dictionary_t::iterator>& v = tmp.at(key);
+    if (v.size() > 1) {
+      std::vector<std::string>& v_res = it->second;
+      for (auto v_it = v.rbegin(); v_it != v.rend(); v_it++) {
+        const auto& _v_it = *v_it;
+        if (_v_it != it) {
+          v_res.push_back(_v_it->first);
+        }
+      }
+    }
+  }
+}
