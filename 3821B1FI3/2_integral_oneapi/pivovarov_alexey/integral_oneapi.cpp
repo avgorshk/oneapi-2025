20fc245df45dd8484353324afb92de8ec1e0a736
pivovarov_alexey <alex6756792@gmail.com>
2025-04-22 23:10:31 +0300

    fix

--- 3821B1FI3/2_integral_oneapi/pivovarov_alexey/integral_oneapi.cpp
+++ 3821B1FI3/2_integral_oneapi/pivovarov_alexey/integral_oneapi.cpp
@@ -1,29 +1,30 @@
 #include "integral_oneapi.h"
-
 #include <range.hpp>
 #include <reduction.hpp>
 
-
 float IntegralONEAPI(float start, float end, int count, sycl::device device) {
-    float res = 0.0f;
-    float interval_len = (end - start) / count;
+    float ans = 0.0f;
+
     {
-        sycl::buffer<float> buf_res(&res, 1);
+        sycl::buffer<float> buf_ans(&ans, 1);
 
         sycl::queue queue(device);
 
         queue.submit([&](sycl::handler& cgh) {
-            auto reduction = sycl::reduction(buf_res, cgh, sycl::plus<>());
+            auto reduction = sycl::reduction(buf_ans, cgh, sycl::plus<>());
 
             cgh.parallel_for(sycl::range<2>(count, count), reduction, [=](sycl::id<2> id, auto& sum) {
-                float x1 = start + interval_len * id.get(0);
-                float x2 = start + interval_len * (id.get(0) + 1);
-                float y1 = start + interval_len * id.get(1);
-                float y2 = start + interval_len * (id.get(1) + 1);
-                sum += sycl::sin((x1 + x2) / 2.0f) * sycl::cos((y1 + y2) / 2.0f) * (x2 - x1) * (y2 - y1);
+                float Xi = start + (end - start) / count * id.get(0);
+                float Xi_1 = start + (end - start) / count * (id.get(0) + 1);
+                float Yi = start + (end - start) / count * id.get(1);
+                float Yi_1 = start + (end - start) / count * (id.get(1) + 1);
+                sum += sycl::sin((Xi + Xi_1) / 2.0f) * sycl::cos((Yi + Yi_1) / 2.0f) * (Xi_1 - Xi) * (Yi_1 - Yi);
                 });
             });
+
         queue.wait();
     }
-    return res;
+
+
+    return ans;
 }
